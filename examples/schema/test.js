bb3f2238cc2e929853c5c68a15c0299e43873164
Michael Schwartz <mykesx@gmail.com>
2014-12-18 14:37:47 -0800

    fix Schema compile error

---
+++ examples/schema/test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mschwartz on 12/18/14.
+ */
+
+var PostgreSQL = require('decaf-postgresql').PostgreSQL,
+Schema         = require('decaf-postgresql').Schema;
+
+
+global.SQL = new PostgreSQL({
+    user     : 'mschwartz',
+    password : '',
+    database : 'test'
+});
+
+Schema.add({
+    name: 'test',
+    fields: [
+        { name: 'a', type: 'int', autoIncrement: true },
+        { name: 'b', type: 'varchar', size: 10 },
+        { name: 'c', type: 'int' }
+    ]
+});
+
+Schema.putOne('test', {
+    b: 'hello',
+    c: 11
+});
+
+console.dir(Schema.find('test', {}));
